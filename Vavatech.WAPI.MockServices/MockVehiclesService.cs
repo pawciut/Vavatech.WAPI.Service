a5b09b18633cc816202f98eef9eb8294d5738595
student-vavatech <student-vavatech@DESKTOP-B473UAV>
2017-10-24 12:50:13 +0200

    vehicles

---
+++ Vavatech.WAPI.MockServices/MockVehiclesService.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Vavatech.WAPI.Models;
+using Vavatech.WAPI.Services;
+
+namespace Vavatech.WAPI.MockServices
+{
+    public class MockVehiclesService : MockItemsService<Vehicle>, IVehiclesService
+    {
+        public MockVehiclesService()
+		: base()
+        {
+        }
+    }
+}
