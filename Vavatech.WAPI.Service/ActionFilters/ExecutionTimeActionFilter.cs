ed7c2df495cb1bdcf4123f987b3b056e42ba98d8
student-vavatech <student-vavatech@DESKTOP-B473UAV>
2017-10-25 10:48:11 +0200

    executiontime impl

--- Vavatech.WAPI.Service/ActionFilters/ExecutionTimeActionFilter.cs
+++ Vavatech.WAPI.Service/ActionFilters/ExecutionTimeActionFilter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Http.Controllers;
@@ -11,12 +12,23 @@ namespace Vavatech.WAPI.Service.ActionFilters
     {
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
+            var actionName = actionContext.ActionDescriptor.ActionName;
+            actionContext.Request.Properties[actionName] = Stopwatch.StartNew();
             base.OnActionExecuting(actionContext);
         }
 
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             base.OnActionExecuted(actionExecutedContext);
+
+            var actionName = actionExecutedContext.ActionContext.ActionDescriptor.ActionName;
+            var stopWatch = actionExecutedContext.ActionContext.Request.Properties[actionName] as Stopwatch;
+            if (stopWatch != null)
+            {
+                stopWatch.Stop();
+                actionExecutedContext.Response.Headers.Add("execution-time", stopWatch.Elapsed.ToString());
+                Trace.WriteLine($"{actionName} elapsed {stopWatch.Elapsed}");
+            }
         }
     }
 }
